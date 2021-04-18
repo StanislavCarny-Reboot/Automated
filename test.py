7ff0bc3586f4884e7ec27346b65a1da4e14c71b1
StanislavCarny-Reboot <73179782+StanislavCarny-Reboot@users.noreply.github.com>
2021-04-18 14:29:45 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ print("hello")
 a = ['a','b','c']
 df = pd.DataFrame(a)
 
-df.to_csv(r'https://raw.githubusercontent.com/StanislavCarny-Reboot/Automated/4e68dc0568ff5c323a2781aec375356c7cc470bf/test1.csv')
+df.to_csv(r'https://raw.githubusercontent.com/StanislavCarny-Reboot/Automated/test1.csv')
 
