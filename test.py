e5e890463869d6b76031d4fe1091de049ed95475
StanislavCarny-Reboot <73179782+StanislavCarny-Reboot@users.noreply.github.com>
2021-04-18 15:19:41 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,34 @@
-import pandas as pd
+# define connection
+con = psycopg2.connect(
+    host ='ec2-54-72-155-238.eu-west-1.compute.amazonaws.com',
+    database = 'd77vu5pdkc80c5',
+    user = 'ziverzcxjkukah',
+    password = '86be3e356b4890952d6dddefc0580644b8e262797eacd19390e90c55b6b66d9b',
+    port = '5432'
+    
+)
 
-print("hello Juraj")
-print("hello")
+# create cursor to enable to execute SQL commands
+cur = con.cursor()
 
-a = ['a','b','c']
-df = pd.DataFrame(a)
+# excecute SQL command
 
-df.to_csv(r'https://raw.githubusercontent.com/StanislavCarny-Reboot/Automated/test1.csv')
+cur.execute(
+"""
+INSERT INTO accounts (user_id,username,password,email,created_on)
+VALUES(2, 'tes2','pass','etes@emal.com','2020-01-01')
+"""
+)
 
+# only for selecting data
+#rows = cur.fetchall()
+
+# commit is only for insert statements
+con.commit()
+
+#print(rows)
+
+    
+cur.close()
+
+con.close()
