2c2f83c32594601c8e762f4e40ca049a015e4711
StanislavCarny-Reboot <73179782+StanislavCarny-Reboot@users.noreply.github.com>
2021-04-19 18:31:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,33 @@
 import psycopg2
+import datetime 
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+test = pd.DataFrame({'Name':['runs']})
+test['time'] = datetime.datetime.now()
+
 
 # define connection
-con = psycopg2.connect(
-    host ='ec2-54-72-155-238.eu-west-1.compute.amazonaws.com',
-    database = 'd77vu5pdkc80c5',
-    user = 'ziverzcxjkukah',
-    password = '86be3e356b4890952d6dddefc0580644b8e262797eacd19390e90c55b6b66d9b',
-    port = '5432'
-    
+connect = "postgresql+psycopg2://%s:%s@%s:5432/%s" % (
+    'ziverzcxjkukah',
+   '86be3e356b4890952d6dddefc0580644b8e262797eacd19390e90c55b6b66d9b',
+    'ec2-54-72-155-238.eu-west-1.compute.amazonaws.com',
+    'd77vu5pdkc80c5',
 )
 
-# create cursor to enable to execute SQL commands
-cur = con.cursor()
-
-# excecute SQL command
-
-cur.execute(
-"""
-INSERT INTO accounts (user_id,username,password,email,created_on)
-VALUES(2, 'tes2','pass1','etes@emal1.com','2020-01-02')
-"""
-)
-
-# only for selecting data
-#rows = cur.fetchall()
-
-# commit is only for insert statements
-con.commit()
-
-#print(rows)
-
+def to_alchemy(df):
+    """
+    Using a dummy table to test this call library
+    """
+    engine = create_engine(connect)
+    df.to_sql(
+        'cron_test', 
+        con=engine, 
+        index=False, 
+        if_exists='replace'
+    )
     
-cur.close()
-
-con.close()
+    
+to_alchemy(test)
+    
