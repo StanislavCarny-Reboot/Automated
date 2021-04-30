989d4d00b3193882a72c4994fe0e36b461d31b82
StanislavCarny-Reboot <73179782+StanislavCarny-Reboot@users.noreply.github.com>
2021-04-30 18:08:56 +0200

    Update scrape_data.py

--- scrape_data.py
+++ scrape_data.py
@@ -22,7 +22,7 @@ three_KK_10M_plus = 'https://www.sreality.cz/api/cs/v2/estates?category_main_cb=
 three_plus_one = 'https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_sub_cb=7&category_type_cb=1&czk_price_summary_order2=10000000%7C10000000000&locality_region_id=10&per_page=999&tms=1617447924801'
 four = 'https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_sub_cb=8%7C9&category_type_cb=1&czk_price_summary_order2=10000000%7C10000000000&locality_region_id=10&per_page=999&tms=1617447981333'
 five_and_more = 'https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_sub_cb=10%7C11%7C12%7C16&category_type_cb=1&czk_price_summary_order2=10000000%7C10000000000&locality_region_id=10&per_page=999&tms=1617448030932'
-apis = [one_kk,one_plus_one,two_KK_7M,two_KK_7M_plus,three_KK_10M,three_KK_10M_plus,three_plus_one,four,five_and_more]
+apis = [one_kk,one_plus_one,two_KK_7M,two_KK_7M_plus,two_plus_one,three_KK_10M,three_KK_10M_plus,three_plus_one,four,five_and_more]
 
 #---------------------------------------------------------------------------------------
 
