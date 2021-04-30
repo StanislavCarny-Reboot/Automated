05955ac9c39dedc68d2ef424af98de9d4ddb54d2
StanislavCarny-Reboot <73179782+StanislavCarny-Reboot@users.noreply.github.com>
2021-04-30 13:40:00 +0200

    Update scrape_data.py

--- scrape_data.py
+++ scrape_data.py
@@ -95,6 +95,7 @@ def add_columns():
     global join
     final['rooms'] = final['name'].apply(lambda x :re.search('[0-9]+\+kk|[0-9]+\+[0-9]',x)[0] if re.search('[0-9]+\+kk|[0-9]+\+[0-9]',x) is not None else 'atyp')
     final['quarter'] = final['locality'].apply(lambda x : x.split('- ')[-1]) 
+    final['Užitná plocha'].astype('int')
     final['Average per Quarter'] = final.groupby(['quarter','rooms'])['price'].transform(np.mean)
     final['Podlaží'] = final['Podlaží'].apply(lambda x : str(x).replace('přízemí',"0."))
     final['Podlaží'] = final['Podlaží'].replace('nan','0')
